d3eb41c1378934ee171d5916bd0755427977f0c5
Keroiw <blazej.wiorek@gmail.com>
2020-06-13 01:13:43 +0200

    Hide distribution plot labels

--- visualisations.py
+++ visualisations.py
@@ -1,29 +1,32 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 
 def show_distributions(theta_a_est, theta_b_est, theta_a, theta_b):
-
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    # cmap = sns.cubehelix_palette(light=1, as_cmap=True)
     cmap = sns.color_palette("YlGnBu")
 
-    def make_heatmap(ax, theta, x_ticks=True):
-        sns.heatmap(theta,
-                    ax=ax,
-                    annot=True,
-                    cmap=cmap,
-                    cbar=False,
-                    xticklabels=x_ticks)
+    def make_heatmap(ax, theta, x_ticks=True, y_ticks=True):
+        theta = pd.DataFrame(theta)
+        theta["Support"] = [str(i) for i in range(1, 5)]
+        theta.set_index("Support", inplace=True)
+        heatmap = sns.heatmap(theta,
+                              ax=ax,
+                              annot=True,
+                              cmap=cmap,
+                              cbar=False,
+                              xticklabels=x_ticks,
+                              yticklabels=y_ticks)
+        if not y_ticks:
+            heatmap.set_ylabel('')
 
     make_heatmap(ax1, theta_a_est, x_ticks=False)
     make_heatmap(ax3, theta_b_est)
-    make_heatmap(ax2, theta_a, x_ticks=False)
-    make_heatmap(ax4, theta_b)
+    make_heatmap(ax2, theta_a, x_ticks=False, y_ticks=False)
+    make_heatmap(ax4, theta_b, y_ticks=False)
 
     ax1.set_title('Estimated')
     ax2.set_title('Theoretical')
 
     plt.plot()
-
-
