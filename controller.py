c5ddc3b65acf15258061c145b15e3be6d4cdfa14
Keroiw <blazej.wiorek@gmail.com>
2020-06-16 23:30:34 +0200

    Add alpha to simulation results

--- controller.py
+++ controller.py
@@ -34,24 +34,28 @@ def single_run(X, max_rep=1000, verbose=False, est_alpha="no", alpha=0.5):
     return theta_1, theta_2, alpha
 
 
-def repeat_experiment(source_file: str, dest_file: str, reps=3, max_rep=1000, est_alpha="no"):
+def repeat_experiment(source_file: str, dest_file: str, reps=3, max_rep=1000, est_alpha="no", alpha=0.5):
     theta_a_lst = []
     theta_b_lst = []
+    alphas_est = []
 
     true_theta_a, true_theta_b = get_thetas(source_file)
     for i in range(reps):
         X = get_data(source_file)["X"].astype(int)
-        theta_a, theta_b, alpha_1, counter = init_em(X, max_rep, est_alpha=est_alpha, alpha=0.5)
+        theta_a, theta_b, est_alpha, counter = init_em(X, max_rep, est_alpha=est_alpha, alpha=alpha)
         theta_a_lst.append(theta_a)
         theta_b_lst.append(theta_b)
+        alphas_est.append(est_alpha)
 
     theta_a_est = reduce(lambda x, y: x + y, theta_a_lst) / len(theta_a_lst)
     theta_b_est = reduce(lambda x, y: x + y, theta_b_lst) / len(theta_b_lst)
+    alpha_est = reduce(lambda x, y: x + y, alphas_est) / len(alphas_est)
 
     res_dict = {"theta_a_est": theta_a_est.tolist(),
                 "theta_b_est": theta_b_est.tolist(),
                 "theta_a": true_theta_a.tolist(),
-                "theta_b": true_theta_b.tolist()}
+                "theta_b": true_theta_b.tolist(),
+                "alpha": alpha_est}
 
     with open(dest_file, 'w') as outfile:
         json.dump(res_dict, outfile)
@@ -63,7 +67,7 @@ def repeat_experiment(source_file: str, dest_file: str, reps=3, max_rep=1000, es
 def read_results(source_file: str):
     with open(source_file, 'r') as input_file:
         params = json.load(input_file)
-    return {k: np.array(v) for k, v in params.items()}
+    return {k: (np.array(v) if "theta" in k else v) for k, v in params.items()}
 
 
 if __name__ == "__main__":
