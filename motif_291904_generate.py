3429017be6c27be35e4981c68e1c67421ca32cb3
Keroiw <blazej.wiorek@gmail.com>
2020-06-17 21:18:19 +0200

    Refactor interface scripts

--- motif_291904_generate.py
+++ motif_291904_generate.py
@@ -6,13 +6,14 @@ import random
 
 def ParseArguments():
     parser = argparse.ArgumentParser(description="Motif generator")
-    parser.add_argument('--params', default="params_set1.json", required=False, help='Plik z Parametrami  (default: %(default)s)')
-    parser.add_argument('--output', default="generated_data.json", required=False, help='Plik z Parametrami  (default: %(default)s)')
+    parser.add_argument('--params', default="./motify_test/params_test.json", required=False, help='Plik z Parametrami  (default: %(default)s)')
+    parser.add_argument('--output', default="./motify_test/gen_test.json", required=False, help='Plik z Parametrami  (default: %(default)s)')
     args = parser.parse_args()
     return args.params, args.output
     
     
 param_file, output_file = ParseArguments()
+
 
 with open(param_file, 'r') as inputfile:
     params = json.load(inputfile)
@@ -23,23 +24,21 @@ k = params['k']
 alpha = params['alpha']
 theta_a = np.asarray(params['Theta'])
 theta_b = np.asarray(params['ThetaB'])
+theta_b = np.repeat(theta_b, w).reshape(4, -1)
 
 
 X = np.zeros((k, w))
 for i in range(k):
     if random.random() < alpha:
         distr = theta_a
-        X[i, :] = [np.random.choice([1, 2, 3, 4], size=1, p=distr[:, i]) for i in range(w)]
     else:
         distr = theta_b
-        X[i, :] = np.random.choice([1, 2, 3, 4], size=w, p=distr)
-
+    X[i, :] = [np.random.choice([1, 2, 3, 4], size=1, p=distr[:, i]) for i in range(w)]
 
 gen_data = {    
     "alpha": alpha,
     "X": X.tolist()
     }
 
-
 with open(output_file, 'w') as outfile:
-    json.dump(gen_data, outfile)
+     json.dump(gen_data, outfile)
